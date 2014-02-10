d66aa81fbcf6ae5138dfdd5c50ef43292f20ff60
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-02-10 23:20:12 +0900

    Add spec

--- spec/out_datadog_spec.rb
+++ spec/out_datadog_spec.rb
@@ -132,4 +132,20 @@ describe Fluent::DatadogOutput do
       d.emit({"metric" => "some.metric.name", "value" => 100.0}, time)
     end
   end
+
+  it 'should be skipped if `metric` key does not exists' do
+    run_driver do |d, dog|
+      dog.should_receive(:emit_points).with(
+        "some.metric.name",
+        [[Time.parse("2014-02-08 04:14:15 UTC"), 50.0],
+         [Time.parse("2014-02-08 04:14:15 UTC"), 100.0]],
+        {"tags"=>["test.default"]}
+      )
+
+      d.emit({"no metric" => "some.metric.name", "value" => 51.0}, time)
+      d.emit({"no metric" => "some.metric.name", "value" => 101.0}, time)
+      d.emit({"metric" => "some.metric.name", "value" => 50.0}, time)
+      d.emit({"metric" => "some.metric.name", "value" => 100.0}, time)
+    end
+  end
 end
